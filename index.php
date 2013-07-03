c0fc782e70c475c2f9189a01b39ace684e157645
Adam Blum <adamblum@adamblummusic.com>
2013-07-02 22:46:48 -0400

    Updated Sass, inserted basic HTML content

--- index.php
+++ index.php
@@ -2,9 +2,76 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style/css/global.css">
 	<title>Responsive Design</title>
 </head>
 <body>
-	
+	<header><p>header</p>
+
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+  </nav>
+ 
+</header>
+<article>
+  <section>
+    <h1>Hey</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus accusamus rem minima excepturi repudiandae qui dolorum nobis autem nisi deleniti amet corporis necessitatibus quis ex ducimus aut maiores nihil?</p>
+  
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus accusamus rem minima excepturi repudiandae qui dolorum nobis autem nisi deleniti amet corporis necessitatibus quis ex ducimus aut maiores nihil?</p>
+    
+    <a href="#.jpg" role="button">Download</a>
+    
+    
+<form action="#">
+
+    <label for="name">Text Input:</label>
+    <input type="text" name="name" id="name" placeholder="John Smith" />
+
+
+  <fieldset>
+    <legend>Radio Button Choice</legend>
+
+    <input type="radio" name="radio-choice" id="radio-choice-1" value="choice-1" />
+    <label for="radio-choice-1">Choice 1</label>
+    
+
+    
+    <input type="radio" name="radio-choice" id="radio-choice-2" value="choice-2" />
+    <label for="radio-choice-2">Choice 2</label>
+  </fieldset>
+
+    <label for="textarea">Textarea:</label>
+    <textarea placeholder="Type your message here" name="textarea" id="textarea"></textarea>
+
+    
+    <input type="checkbox" name="checkbox" id="checkbox" />
+    <label for="checkbox">Checkbox</label>
+    
+    <input type="checkbox" name="checkbox2" id="checkbox2" />
+    <label for="checkbox2">Checkbox</label>
+  
+    <input role="button" type="submit" value="Submit" />
+
+</form>  
+  <!--
+    <section role="marquee">
+      <img src="http://www.wallpapersonview.com/wallpapers/4/landscape_wonderful_nature_quality_sunset_background_picture-115.jpg" />
+      <img src="http://www.free-pictures-photos.com/fireworks/picture-fireworks-033.jpg" />
+      <img src="http://placehold.it/1600x900&text=third" />
+    </section>
+    -->
+    
+  </section>
+    <nav>
+      
+hello!
+      
+    </nav>
+    <aside>aside</aside>
+ </article>
+ <footer>footer</footer>
 </body>
 </html>
